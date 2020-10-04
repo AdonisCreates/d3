94450d22a96e0b86ace6ad7ad4e8bc4d2269218c
Adriel Adonis <arodriguez32@fisher.edu>
2020-10-04 11:28:04 -0400

    made box for charts

---
+++ app.js
@@ -0,0 +1,17 @@
+const FAKE_DATA = [
+    { id: 'bitcoin', value: 10021},
+    { id: 'etherum', value: 499},
+    { id: 'litecoin', value: 40},
+    { id: 'tether', value: 1},
+    { id: 'bitcoin cash', value: 200},
+    { id: 'xrp', value: 1},
+    { id: 'wrapped bitcoin', value: 100241},
+    { id: 'polkadot', value: 4499},
+    { id: 'chainlin', value: 9},
+
+];
+
+
+d3.select('div')
+    .classed('container', true)
+    .style('border', '10px solid red');
