5f3623a0fc831f7b6bf11632a8cfbda2234a9fab
Adriel Adonis <arodriguez32@fisher.edu>
2020-10-04 22:17:34 -0400

    d3 charting looking very very nice

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const FAKE_DATA = [
-    { id: 'bitcoin', value: 1},
-    { id: 'etherum', value: 2},
-    { id: 'litecoin', value: 3},
-    { id: 'tether', value: 4},
-    { id: 'bitcoin cash', value: 5},
-    { id: 'xrp', value: 1},
-    { id: 'wrapped bitcoin', value: 7},
-    { id: 'polkadot', value: 8},
-    { id: 'chainlin', value: 9},
-
+    { id: 'bitcoin', value: 10, region: 'USA'},
+    { id: 'etherum', value: 9, region: 'Brazil'},
+    { id: 'litecoin', value: 8, region: 'Eggs'},
+    { id: 'tether', value: 10, region: 'Boom'},
 ];
 
+// same width for all bars; uniform distrubution 
+//range shows what space is available
+const xScale = d3
+    .scaleBand()
+    .domain(FAKE_DATA.map((dataPoint) => dataPoint.region))
+    .rangeRound([0, 250])
+    .padding(0.1);
+const yScale = d3.scaleLinear().domain([0, 15]).range([200, 0]);
 
 const container = d3.select('svg')
     .classed('container', true);
@@ -21,9 +23,10 @@ const bars = container
     .enter()
     .append('rect')
     .classed('bar', true)
-    .attr('width', 50)
-    .attr('height', data => (data.value * 15));
-
+    .attr('width', xScale.bandwidth())
+    .attr('height', (data) => 200 - yScale(data.value))
+    .attr('x', data => xScale(data.region))
+    .attr('y', data => yScale(data.value));
     // cool things to know
     // you cant use justifiy content or background use fill
     // you dont use div you use svg
